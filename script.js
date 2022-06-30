8b141b16556343c3dd5ba411ccfcb5a7687261c7
Igor Gjorgjievski <igor.m.gjorgjievski@gmail.com>
2022-06-30 13:54:57 +0200

    finished the challenge

---
+++ script.js
@@ -0,0 +1,71 @@
+// first exercise
+let number = +prompt("enter a number");
+
+if (number % 2 === 0) {
+  console.log(`number ${number} is even`);
+} else {
+  console.log(`number ${number} is odd`);
+}
+// end of first exercise
+
+// second exercise
+
+for (let i = 10; i <= 100; i++) {
+  if (i % 6 === 0) {
+    console.log(i);
+  }
+}
+
+// end of second exercise
+
+// third exercise
+let arr = [15, 13, 20];
+function findSmallestNumber(arr) {
+  let howLarge = arr[0];
+  let bigOrSmall = "smallest";
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < howLarge) {
+      howLarge = arr[i];
+    }
+  }
+  primeOrNot(howLarge, bigOrSmall);
+}
+function findLargerstNumber(arr) {
+  let howLarge = 0;
+  let bigOrSmall = "biggest";
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > howLarge) {
+      howLarge = arr[i];
+    }
+  }
+  primeOrNot(howLarge, bigOrSmall);
+}
+function primeOrNot(howLarge, bigOrSmall) {
+  let isPrime = true;
+  if (howLarge === 1) {
+    console.log("1 is neither prime nor composite number.");
+  } else if (howLarge > 1) {
+    // looping through 2 to number-1
+    for (let i = 2; i < howLarge; i++) {
+      if (howLarge % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) {
+      console.log(
+        `the ${bigOrSmall} number is ${howLarge} and is a prime number`
+      );
+    } else {
+      console.log(
+        `the ${bigOrSmall} number is ${howLarge} and is not a prime number`
+      );
+    }
+  }
+}
+
+findLargerstNumber(arr);
+findSmallestNumber(arr);
+
+// end of third exercise
